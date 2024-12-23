a2c877fd70f035ffadcee6917343de9d17cf4ea2
harsh-bhoir-01 <Shwetabhoir27@gmail.com>
2024-12-23 12:38:54 +0530

    fixed errors

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -7,7 +7,7 @@ import { Appbar } from "../components/Appbar";
 export const Blog = () => {
   const { id } = useParams();
   const { loading, blog } = useBlog({ id: id || "" });
-  if (loading) {
+  if (loading || !blog) {
     return (
       <div>
         <Appbar />
